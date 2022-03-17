4f128a425d997506ead77fbb6a79b0ceeb4c9d60
AlexJob <henaochacon@gmail.com>
2022-03-16 23:07:01 -0500

    Slider customer reviews

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let toggleMenu = document.getElementById('toggle-menu'),
+const toggleMenu = document.getElementById('toggle-menu'),
     contentMenu = document.getElementById('content_menu'),
-    suscribeBtn = document.getElementById('suscribe'),
-    imgMenu = document.getElementById('img_menu');
+    imgMenu = document.getElementById('img_menu'),
+    slider = document.querySelector('#slider'),
+    btnLeft = document.querySelector('#btn--left'),
+    btnRight = document.querySelector('#btn--right');
+
+let suscribeBtn = document.getElementById('suscribe'),
+    sendBtn = document.querySelector('#btn_send'),
+    sliderSection = document.querySelectorAll('.slider__section'),
+    sliderSectionLast = sliderSection[sliderSection.length - 1];
 
 toggleMenu.addEventListener('click', () => {
     contentMenu.classList.toggle('show_menu');
@@ -22,3 +29,36 @@ contentMenu.addEventListener('click', e => {
 suscribeBtn.addEventListener('', (e) => {
     e.preventDefault();
 });
+
+sendBtn.addEventListener('', (e) => {
+    e.preventDefault();
+});
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = '0.5s linear';
+    setTimeout(function () {
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
+function Prev() {
+    let sliderSection = document.querySelectorAll('.slider__section'),
+        sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = '0%';
+    slider.style.transition = '0.5s linear';
+    setTimeout(function () {
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = '-100%';
+    }, 500);
+}
+
+btnRight.addEventListener('click', function () { Next(); });
+btnLeft.addEventListener('click', function () { Prev(); });
+
+// setInterval(function () { Next(); }, 7000);
